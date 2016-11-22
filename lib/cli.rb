f90568cd2f0454f4d0e322c27ef8f9eeb37049fd
Simon Woolf <simon@ably.io>
2016-11-22 13:27:58 +0000

    Add local mode for when have no internet

--- lib/cli.rb
+++ lib/cli.rb
@@ -5,7 +5,12 @@ require_relative 'controller'
 # Set title of the terminal
 system("printf \"\033]0;tt\007\"")
 
-controller = Controller.spawn(:controller)
+options = {}
+if ARGV.include?("-l") || ARGV.include?("--local")
+  options[:mode] = :local
+end
+
+controller = Controller.spawn(:controller, options)
 
 Signal.trap("WINCH") do
   Thread.new do
@@ -29,5 +34,9 @@ loop do
     controller.tell(:procrastinating)
   when ' '
     controller.tell(:refresh)
+  when 'l'
+    controller.tell(:local_mode)
+  when 'i'
+    controller.tell(:internet_mode)
   end
 end
--- lib/controller.rb
+++ lib/controller.rb
@@ -16,9 +16,7 @@ def p(str)
 end
 
 class Controller < Concurrent::Actor::Context
-  def initialize()
-    @bee = Beemind.spawn(:bee)
-
+  def initialize(options)
     @timer = Concurrent::TimerTask.new(execution_interval: PERIOD_SECS, run_now: true) {
       self.tell(:tick)
     }
@@ -26,10 +24,16 @@ class Controller < Concurrent::Actor::Context
 
     @state = {
       status: :initialized,
+      mode: options[:mode] || :internet,
       periods_in_state: 0,
       work_pomodoro_periods: 0,
+      accumulated_work_pomodoros: 0,
       prompt: nil
     }
+
+    if @state[:mode] == :internet
+      @bee = Beemind.spawn(:bee)
+    end
   end
 
   def on_message(msg, payload=nil)
@@ -45,8 +49,22 @@ class Controller < Concurrent::Actor::Context
       show_update
 
     when :info
-      @state[:prompt] << "\n"
-      @state[:prompt] << payload
+      @state[:prompt] = "\n#{payload}"
+      show_update
+
+    when :local_mode
+      @state[:mode] = :local
+      @state[:prompt] = "(local mode)"
+      show_update
+
+    when :internet_mode
+      @state[:mode] = :internet
+      @bee ||= Beemind.spawn(:bee)
+      accumulated_work_pomodoros.times do
+        @bee.tell(:submit_work_pomodoro)
+      end
+      @state[:accumulated_work_pomodoros] = 0
+      @state[:prompt] = "(internet mode)"
       show_update
 
     when Array
@@ -69,7 +87,7 @@ class Controller < Concurrent::Actor::Context
     if initialized?
       p "time tracker".white.bold
     else
-      p "#{status}: #{periods_to_minutes(periods_in_state)} minutes#{pomodoro_proportion}".bold.send(state_color)
+      p "#{status}: #{periods_to_minutes(periods_in_state)} minutes#{pomodoro_proportion}#{accumulation}".bold.send(state_color)
     end
 
     if prompt
@@ -85,6 +103,14 @@ class Controller < Concurrent::Actor::Context
     end
   end
 
+  def accumulation
+    if mode == :local
+      " (accumulated: #{accumulated_work_pomodoros})"
+    else
+      ''
+    end
+  end
+
   def on_tick
     @state[:periods_in_state] += 1
 
@@ -93,8 +119,12 @@ class Controller < Concurrent::Actor::Context
     end
 
     if work_pomodoro_periods >= PERIODS_PER_POMODORO
-      @bee.tell(:submit_work_pomodoro)
       @state[:work_pomodoro_periods] = 0
+      if mode == :internet && @bee
+        @bee.tell(:submit_work_pomodoro)
+      else
+        @state[:accumulated_work_pomodoros] += 1
+      end
     end
 
     if counting_pomodoros? && periods_in_state >= PERIODS_PER_POMODORO && !prompt
