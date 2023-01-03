16aaa65219686083133ac3406d26abbb46abc82d
Simon Woolf <simon.woolf@ably.com>
2023-01-03 18:33:10 +0000

    Implement work duration log file

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,4 +1,5 @@
 require 'active_support/core_ext/numeric/time'
+require 'date'
 
 def periods_to_minutes(periods)
   (periods * PERIOD_SECS) / 60
@@ -11,7 +12,8 @@ end
 def periods_to_hmstr(periods)
   hours, minutes = periods_to_minutes(periods).divmod(60)
   if hours == 0
-    "#{minutes}m"
+    # pad with leading zero to make the length output monotonic - makes file output easier
+    "#{minutes.to_s.rjust(2, '0')}m"
   else
     "#{hours}h#{minutes}m"
   end
@@ -23,6 +25,7 @@ PERIODS_PER_BREAK = minutes_to_periods(10)
 WORK_PERIODS_TO_SAT = minutes_to_periods(4 * 60)
 WORK_PERIODS_TO_OVERSAT = minutes_to_periods(8 * 60)
 DING_SOUND = '/home/simon/dev/dotfiles/pomodoro-finish.wav'
+WORK_DURATION_BY_DAY_FILEPATH = '/home/simon/work-log'
 DING_SPEED = 3
 SLOW_DING_SPEED = 2
 
@@ -41,9 +44,33 @@ class Controller < Concurrent::Actor::Context
     @leisure_pomodoro_periods = 0
     @prompt = []
 
+    init_work_by_day_file
+
     @outputs = options[:outputs].map do |output|
       output.spawn(output.to_s)
     end
+  end
+
+  def init_work_by_day_file
+    f = @work_by_day_file = File.open(WORK_DURATION_BY_DAY_FILEPATH, 'r+')
+    today = Date.today.to_s
+    date_length = 10
+    lines = f.readlines
+    if lines.length > 0 && lines.last.start_with?(today)
+      init_work_pom_periods(lines.last.slice(date_length + 1, 99))
+      # seek to the beginning of the value of the last line
+      @work_by_day_file_pos = f.pos - lines.last.length + date_length + 1
+    else
+      # no existing date
+      f.write("\n#{today} ")
+      @work_by_day_file_pos = f.pos
+    end
+  end
+
+  def init_work_pom_periods(hmstr)
+    match_data = hmstr.match(/(?:(\d{1,2})h)?(\d{2})m/)
+    h, m = match_data.captures
+    @work_pomodoro_periods = minutes_to_periods((h || '0').to_i * 60 + m.to_i)
   end
 
   def on_message(msg, payload=nil)
@@ -172,6 +199,17 @@ class Controller < Concurrent::Actor::Context
       play_ding(slow: true)
       show_notification('Oversatisfied obligation')
     end
+
+    write_work_by_day_to_file
+  end
+
+  def write_work_by_day_to_file
+    # overwrite any previous timestring (length of the hmstr is monotonically
+    # increasing so don't worry about leaving anything at the end)
+    f = @work_by_day_file
+    f.seek(@work_by_day_file_pos)
+    f.write(periods_to_hmstr(@work_pomodoro_periods))
+    f.flush
   end
 
   def cls
